39177d5b6b6a083b1dd6ca2b83b4b3849f49084b
smgrizzlybear <iamsandipan158@gmail.com>
2025-09-16 12:50:28 +0530

    changes

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print("hello yesss")
+from picamera2 import Picamera2, Preview
+import time
+
+picam2 = Picamera2()
+picam2.start_preview(Preview.QTGL)  # Open preview window
+picam2.start()
+time.sleep(10)  # keep preview open for 10 sec
+picam2.stop()
